72232e5d2d5209976e246c124201a161bf218d9f
Tina Gao <thgao@uwaterloo.ca>
2020-05-20 11:46:28 -0400

    updating frontend

---
+++ client/build/precache-manifest.181a96db3ddebbced8426eb54e9174ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8483f203fde08b8935a9a99d7332d9c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d7c8b8e6c3b147b32f9",
+    "url": "/static/js/2.38368e58.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.38368e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034b67d7fdcebbce4bc7",
+    "url": "/static/js/main.a1949a0c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
