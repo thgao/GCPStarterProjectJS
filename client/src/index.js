3837af4917fa2531f0a027f798f137158ddbfc5f
Tina Gao <thgao@uwaterloo.ca>
2020-05-20 13:50:31 -0400

    wip

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import FoodFinder from './FoodFinder';
+import { CollectorExporter } from '@opentelemetry/exporter-collector';
+import { SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { DocumentLoad } from '@opentelemetry/plugin-document-load';
+import { WebTracerProvider } from '@opentelemetry/web';
+
+// Edit this to point to the app to the OpenTelemetry Collector address:
+// If running locally use http://localhost:55678/v1/trace
+const collectorURL = 'http://localhost:55678/v1/trace';
+// const collectorURL = 'http://35.188.162.236/v1/trace';
+
+const webTracer = new WebTracerProvider({
+  plugins: [
+    new DocumentLoad(),
+  ],
+});
+const collectorOptions = {
+  url: collectorURL,
+};
+const exporter = new CollectorExporter(collectorOptions);
+webTracer.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
 ReactDOM.render(
 	<Router>
